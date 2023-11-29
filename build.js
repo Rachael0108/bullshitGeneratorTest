e64d6b2b9531d6d111fc60798a57c3c8a68e0391
wei_ying <weiy@iclockwork.com>
2023-11-29 18:19:42 +0800

    c: 打包

--- build.js
+++ build.js
@@ -1,16 +1,25 @@
 import {build} from 'esbuild';
 
-build({
+// build({
+//   entryPoints: ['./browser/index.js'],
+//   outfile: './dist/bullshit-generator.browser.js',
+//   bundle: true,
+//   minify: true,
+// });
+//
+// build({
+//   entryPoints: ['./index.js'],
+//   outfile: './dist/bullshit-generator.cjs',
+//   format: "cjs",
+//   bundle: true,
+//   external: ['require', 'fs', 'path', 'url'],
+// });
+const buildOptions = {
   entryPoints: ['./browser/index.js'],
-  outfile: './dist/bullshit-generator.browser.js',
+  outfile: './dist/index.js',
   bundle: true,
+  globalName: 'bullshitGenerator',
   minify: true,
-});
+};
 
-build({
-  entryPoints: ['./index.js'],
-  outfile: './dist/bullshit-generator.cjs',
-  format: "cjs",
-  bundle: true,
-  external: ['require', 'fs', 'path', 'url'],
-});
+build(buildOptions)
